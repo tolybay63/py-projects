d944e4b1b1bd57a3afbbf8008623ab9edaf6dc89
Tolybay.Kuanov <tolybay.zh@gmail.com>
2026-08-20 13:27:33 +0500

    add && upd && del

---
+++ calc-fastapi/db_utils.py
@@ -0,0 +1,92 @@
+
+# Хранилище пулов: {'db_name': pool_object}
+pools = {}
+
+from dotenv import load_dotenv
+import os
+import asyncpg
+
+load_dotenv()
+
+
+# Убедись, что переменные окружения загружены (например, через load_dotenv())
+# from dotenv import load_dotenv
+# load_dotenv()
+
+async def get_db_pool(db_name: str):
+    """Возвращает существующий пул или создает новый"""
+    if db_name not in pools:
+        # Получаем базовый DSN из переменных окружения
+        base_dsn = os.getenv("POSTGRES_DSN")
+
+        if not base_dsn:
+            raise ValueError("Переменная POSTGRES_DSN не найдена в .env файле")
+
+        # Формируем полный DSN: добавляем слэш и имя базы
+        dsn = f"{base_dsn}/{db_name}"
+
+        # Инициализируем пул
+        pools[db_name] = await asyncpg.create_pool(dsn, min_size=1, max_size=10)
+        print(f"--- Пул соединений для БД '{db_name}' создан ({base_dsn}/{db_name}) ---")
+
+    return pools[db_name]
+
+
+async def select_query(sql: str, params: dict, db_name: str):
+    """Выполняет запрос в указанной БД, используя её пул"""
+    pool = await get_db_pool(db_name)
+
+    async with pool.acquire() as conn:
+        values = list(params.values())
+        rows = await conn.fetch(sql, *values)
+        return [dict(row) for row in rows]
+
+#Частный случай УДАЛИТЬ!
+async def get_prop_params(props: list):
+    """Возвращает словарь из props: {'Prop_A': 1000, 'Prop_B: 1001', ...}"""
+    query = "SELECT id, cod FROM prop WHERE cod like 'Prop_%'"
+    res = await select_query(query, {}, "dtj_model")
+    result_dict = {item['cod']: item['id'] for item in res}
+    return {k: result_dict[k] for k in props if k in result_dict}
+
+"""Возвращает словарь {cod: id} из кодов (cods) сущности (entity) в порядке cods: {'Cod_A': 1000, 'Cod_B: 1001', ...}"""
+async def cod_id_from_entity(entity: str, cods: list):
+    query = f"SELECT id, cod FROM {entity} WHERE cod like '{entity}_%'"
+    res = await select_query(query, {}, "dtj_model")
+    result_dict = {item['cod']: item['id'] for item in res}
+    return {k: result_dict[k] for k in cods if k in result_dict}
+
+"""Возвращает словарь {cod: id} из сущности (entity) {'Cod_A': 1000, 'Cod_B: 1001', ...}"""
+async def cod_id_from_entity_map(entity: str, cod: str):
+    query = f"SELECT id, cod FROM {entity} WHERE cod like '{cod}'"
+    res = await select_query(query, {}, "dtj_model")
+    return {item['cod']: item['id'] for item in res}
+
+async def id_propval(entity: str, id_entity: int, cod_prop: str):
+    query = f"""
+        select pv.id, pv.prop from PropVal pv, Prop p
+        where pv.prop=p.id and pv.{entity}={id_entity} and p.cod like '{cod_prop}'    
+    """
+    res = await select_query(query, {}, "dtj_model")
+    if len(res) > 0:
+        return res[0]["id"]
+    else:
+        raise 'NotFoundPossibleValues-{cod_prop}'
+
+async def map_entity_id_from_pv(entity: str, cod_prop: str, key_is_propval: bool = False):
+    query = f"""
+        select pv.id, pv.factorVal from PropVal pv, Prop p
+        where pv.prop=p.id and p.cod='{cod_prop}' and pv.{entity} is not null    
+    """
+    res = await select_query(query, {}, "dtj_model")
+    if key_is_propval:
+        return {item['id']: item[entity] for item in res}
+    else:
+        return {item[entity]: item['id'] for item in res}
+
+
+async def close_all_pools():
+    """Закрывает все открытые пулы при выключении"""
+    for name, pool in pools.items():
+        await pool.close()
+        print(f"--- Пул БД '{name}' закрыт ---")
---
+++ calc-fastapi/main.py
@@ -0,0 +1,97 @@
+from contextlib import asynccontextmanager
+import uvicorn
+from fastapi import FastAPI
+
+from db_utils import close_all_pools, select_query
+
+
+# Определяем логику жизненного цикла
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # --- Код здесь выполняется ПРИ СТАРТЕ ---
+    print(">>> Сервер FastAPI запущен (Lifespan)")
+
+    yield  # Здесь приложение работает и принимает запросы
+
+    # --- Код здесь выполняется ПРИ ОСТАНОВКЕ ---
+    await close_all_pools()
+    print("<<< Сервер FastAPI остановлен, все соединения разорваны (Lifespan)")
+
+
+# Передаем lifespan в конструктор FastAPI
+app = FastAPI(
+    lifespan=lifespan
+)
+
+@app.get("/")
+def read_root():
+    return {"message": "Добро пожаловать в Fast API!"}
+
+
+
+
+@app.get("/factors", tags=["Модель [Meta]: Факторы"], summary="Список факторов")
+async def factors():
+    query = "SELECT * FROM factor WHERE 0=0"
+    # Запрос к базе 'fish_model'
+    data = await select_query(query, {},"fish_model")
+    return data
+
+@app.get("/factor", tags=["Модель [Meta]: Факторы"], summary="Указанный фактор")
+async def factor(id: int=1000):
+    query = "SELECT * FROM factor WHERE id = $1"
+    # Запрос к базе 'fish_model'
+    data = await select_query(query, {"id": id},"fish_model")
+    return data
+
+
+
+@app.get("/factor_vals_by_cod/{cod_factor}", tags=["Факторы"], summary="Список значений указанного фактора")
+async def factor_vals(cod_factor: str="Factor_Defects"):
+    query = f"""
+            select fv.id, fv.cod, fv.name
+            from Factor fv
+                     join Factor f on fv.parent = f.id
+            where f.cod = $1
+            order by fv.ord
+    """
+
+    data = await select_query(query, {"cod_factor": cod_factor}, "fish_model")
+    return data
+
+
+@app.get("/load_obj/{cls}", tags=["Объекты"], summary="Список объектов указанного класса")
+async def load_obj(cls: int=1008):
+    query = f"""
+        select o.id, v.name, o.cod 
+        from Obj o, ObjVer v
+        where o.id=v.ownerVer and v.lastVer=1 and o.cls=$1    
+    """
+    data = await select_query(query, {"cls": cls}, "fish_nsi")
+    return data
+
+
+
+@app.get("/load_dict/{dict_name}", tags=["Словари"], summary="Список значений указанного словаря")
+async def factor_vals(dict_name: str="fd_accesslevel"):
+    query = f"""
+        select id, text 
+        from {dict_name} 
+        where vis=1
+        order by ord
+    """
+    data = await select_query(query, {}, "fish_model")
+    return data
+
+
+@app.get("/users", tags=["Модель [Admin]: Пользователи"], summary="Список пользователей")
+async def users():
+    query = "SELECT * FROM authuser WHERE 0=0"
+    # Запрос к базе 'dtj_admin'
+    # Пул для нее создастся автоматически при первом вызове
+    data = await select_query(query, {},"dtj_admin")
+    return data
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=False)
